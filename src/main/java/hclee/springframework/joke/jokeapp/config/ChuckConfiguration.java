c3a2897dd83ce9f0dd569317c53e1dc66df8b59d
ihyecheon <ihyecheon@gmail.com>
2018-01-28 19:08:36 +0900

    spring configuration
    설정..~!

---
+++ src/main/java/hclee/springframework/joke/jokeapp/config/ChuckConfiguration.java
@@ -0,0 +1,13 @@
+package hclee.springframework.joke.jokeapp.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class ChuckConfiguration {
+//    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
